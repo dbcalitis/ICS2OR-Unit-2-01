919fd238f239bf4f8e9ca46bd2ed969b367a2e86
Ubuntu <ubuntu@ip-172-31-88-46.ec2.internal>
2020-03-06 23:55:52 +0000

    added index.html, jsScript.js and _config.yaml

---
+++ jsScript2.js
@@ -0,0 +1,10 @@
+// JavaScript File
+var text = "Hello World!";
+var text2 = "Goodbye, World";
+var img = document.createElement("img");
+ 
+img.src = "./images/download.jpg";
+
+document.write(text.fontcolor("red"));
+document.body.appendChild(img);
+document.write(text2.fontcolor("blue"));
